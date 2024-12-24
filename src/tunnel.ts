6a78ecb29f5ecb43d160d19fb999e512350c6b93
Matheus Nogueira <matheus.nogueira2008@gmail.com>
2024-12-23 22:21:52 -0300

    default to port 80

--- src/tunnel.ts
+++ src/tunnel.ts
@@ -2,7 +2,7 @@ export async function startTunnel(localEndpoint: string) {
   const { tunnelmole } = await import('tunnelmole');
   const port = new URL(localEndpoint).port;
   const url = await tunnelmole({
-    port: Number(port)
+    port: Number(port) || 80
   });
 
   return url;
