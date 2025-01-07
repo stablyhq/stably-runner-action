6fa3a999fe48835d1ed0a5e7eb9d4707d522710b
Matheus Nogueira <matheus.nogueira2008@gmail.com>
2025-01-06 23:46:01 -0300

    feat: enable local testing (#47)
    * enable local testing
    
    * fix import
    
    * fix lint
    
    * remove branch filter
    
    * fix arg name
    
    * add debug logs
    
    * fix workflow
    
    * debug
    
    * info instead of log
    
    * replace sse
    
    * disable github comment
    
    * build
    
    * use correct field
    
    * missing file
    
    * fix message
    
    * fix url
    
    * debug
    
    * fix log
    
    * quiet mode
    
    * quite mode
    
    * log
    
    * build
    
    * better log
    
    * test
    
    * set api key
    
    * fix auth header
    
    * add content type
    
    * fix
    
    * default to port 80
    
    * bundle
    
    * remove quiet mode
    
    * use docker compose
    
    * debug
    
    * exit the action after done
    
    * github comment
    
    * add github comment to example
    
    * permissions
    
    * fix lint
    
    * lint errors
    
    * remove console.info
    
    * use correct parameter in action
    
    * support async mode
    
    * use npm package
    
    * fix tests
    
    * fix import
    
    * update sdk to 1.0.2
    
    * use test-suite-id in github action
    
    * use tunnel from sdk
    
    * add comments back
    
    * bundle
    
    * Apply suggestions from code review
    
    Co-authored-by: Neil <4138956+nwparker@users.noreply.github.com>
    
    * pull request suggestions
    
    * fix CI and rename PR action
    
    * handle ports in replacement url
    
    * bundle
    
    * add documentation on how to use the action to test localhost
    
    * update authentication header
    
    * bundle
    
    * update sdk to 1.0.4
    
    * use @action/http-client
    
    * bundle
    
    * fix linting
    
    * update runner-sdk to 1.0.5
    
    * bundle
    
    * bump runner-sdk to 1.0.6
    
    * use bearer crendetial handler
    
    * bundle
    
    ---------
    
    Co-authored-by: Neil <4138956+nwparker@users.noreply.github.com>

---
+++ src/api.ts
@@ -0,0 +1,53 @@
+import { HttpClient } from '@actions/http-client';
+import { BearerCredentialHandler } from '@actions/http-client/lib/auth';
+
+const API_ENDPOINT = 'https://api.stably.ai';
+
+type RunTestResponse = {
+  projectId: string;
+  testSuiteRunId: string;
+  testSuiteName: string;
+  results: {
+    testId: string;
+    testName: string;
+    success: boolean;
+  }[];
+};
+
+type RunTestOptions = {
+  urlReplacement?: { original: string; replacement: string };
+  asyncMode?: boolean;
+};
+
+export async function runTestGroup(
+  testSuiteId: string,
+  apiKey: string,
+  options: RunTestOptions
+): Promise<{ statusCode: number; execution?: RunTestResponse }> {
+  const httpClient = new HttpClient('github-action', [
+    new BearerCredentialHandler(apiKey)
+  ]);
+
+  const body = options.urlReplacement
+    ? { urlReplacements: [options.urlReplacement] }
+    : {};
+
+  const url = new URL(`/v1/testSuite/${testSuiteId}/run`, API_ENDPOINT).href;
+  const apiCallPromise = httpClient.post(url, JSON.stringify(body), {
+    'Content-Type': 'application/json'
+  });
+
+  if (!options.asyncMode) {
+    const response = await apiCallPromise;
+    const result = await response.readBody();
+    const resultJson = JSON.parse(result);
+
+    return {
+      statusCode: response.message.statusCode || 0,
+      execution: resultJson
+    };
+  }
+
+  // in async mode, we don't wait for the response, so we consider it's Ok
+  return { statusCode: 200 };
+}
